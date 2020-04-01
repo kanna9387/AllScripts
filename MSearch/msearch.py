1ae9ce0a8527ceb02828f185ae4695df3ff4d68e
kanna9387 <48581437+kanna9387@users.noreply.github.com>
2020-04-01 12:52:47 +0530

    Update msearch.py

--- MSearch/msearch.py
+++ MSearch/msearch.py
@@ -3,7 +3,24 @@ import socket
 msg = \
     'M-SEARCH * HTTP/1.1\r\n' \
     'HOST: 239.255.255.250:1800\r\n' \
+	'ST:upnp:rootdevice\r\n' \
+    'MX: 10\r\n' \
+    'MAN: \"ssdp:discover\"\r\n'
+
+# Set up UDP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+s.settimeout(5)
+s.sendto(bytes(msg.encode()),('239.255.255.250',1800))
+a=None;
+fo=open('links.txt','w')
+fo.close()
+try:
+    while True:
+        data, addr = s.recvfrom(6557)
+        print (addr, data)
+        a = str(data)
         fo=open('links.txt','a')
+        fo.write(a + '\n')
         fo.close()
         
 except socket.timeout:
